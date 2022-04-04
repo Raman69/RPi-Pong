07244d8445cf877a6811fb361e76af7509c6a9d1
MonGoose4381 <ryanwindscheffel@gmail.com>
2022-04-04 15:01:30 +0100

    me

--- Ryan.py
+++ Ryan.py
@@ -1,18 +1,61 @@
 from select import select
 from sense_hat import SenseHat
 import os
-import pygame 
+import pygame
 from pygame.locals import *
 from time import sleep
+
+# Defining LED matrix and clearing sense HAT
 matrix = SenseHat()
-matrix.rotation = 180
+matrix.clear()
+matrix.lowlight = True
 
-def clear():
-    matrix.clear()
+white = (255, 255, 255)
+bat_y = 4
+ball_position = [3, 3]
+ball_velocity = [1, 1]
 
-def set_pixel(a,b,c):
-    matrix.set_pixel(a,b,c)
+# Functions
+def draw_bat():
+    matrix.set_pixel(0, bat_y + 1, white)
+    matrix.set_pixel(0, bat_y, white)
+    matrix.set_pixel(0, bat_y - 1, white)
 
+
+def move_up(event):
+    global bat_y
+    if event.action == "pressed" and bat_y > 1:
+        bat_y -= 1
+
+
+def move_down(event):
+    global bat_y
+    if event.action == "pressed" and bat_y < 6:
+        bat_y += 1
+
+# Main loop
+matrix.stick.direction_up = move_up
+matrix.stick.direction_down = move_down
+while True:
+    draw_bat()
+    sleep(0.25)
+    matrix.clear(0, 0, 0)
+
+def tw(n):
+    r = open("led.txt")
+    lines = r.readlines()
+    w = open('led.txt','w')
+    write = w.writelines(n)
+    a = open('led.txt','a')
+    a = a.writelines(n)
+    r = open("led.txt")
+    lines = r.readline()
+
+while True:
+    for i in range(0,7):
+        if ball_position < [7,i]:
+            print("Ball next")
+            tw(0)
 
 
 
