394b95fccb4187bc394c3c495a811a4c8ceec979
MonGoose4381 <ryanwindscheffel@gmail.com>
2022-04-04 14:48:15 +0100

    mo

--- Ryan.py
+++ Ryan.py
@@ -4,14 +4,21 @@ import os
 import pygame 
 from pygame.locals import *
 from time import sleep
+matrix = SenseHat()
+matrix.rotation = 180
+
 def clear():
     matrix.clear()
 
 def set_pixel(a,b,c):
     matrix.set_pixel(a,b,c)
 
-matrix = SenseHat()
-matrix.rotation = 180
+
+
+
+
+
+
 
 
 
