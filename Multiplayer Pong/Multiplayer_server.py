22984fabc03145f50eae759e45624285dd433a11
Raman69 <abdelrahmanelrefaey5@gmail.com>
2022-04-15 17:46:22 +0100

    Refined Multiplayer Server

--- Multiplayer Pong/Multiplayer_server.py
+++ Multiplayer Pong/Multiplayer_server.py
@@ -6,11 +6,12 @@ import os
 
 opp_bat_y = 4
 
+
 def data_received(data):
     print("recv - {}".format(data))
     global opp_bat_y
     opp_bat_y = int(data)
-    #server.send(data)
+    # server.send(data)
 
 
 def client_connected():
@@ -26,8 +27,8 @@ def client_connected():
     bat_y = 4
     ball_position = [0, 0]
     ball_velocity = [1, 1]
-    w = [255,255,255]
-    x = [0,0,0]
+    w = [255, 255, 255]
+    x = [0, 0, 0]
     sad = [
         x,x,x,x,x,x,x,x,
         x,w,w,x,x,w,w,x,
@@ -36,6 +37,16 @@ def client_connected():
         x,x,x,x,x,x,x,x,
         x,x,w,w,w,w,x,x,
         x,w,w,x,x,w,w,x,
+        x,x,x,x,x,x,x,x
+        ]
+    happy = [
+        x,x,x,x,x,x,x,x,
+        x,w,w,x,x,w,w,x,
+        x,w,w,x,x,w,w,x,
+        x,x,x,x,x,x,x,x,
+        x,x,x,x,x,x,x,x,
+        x,w,w,x,x,w,w,x,
+        x,x,w,w,w,w,x,x,
         x,x,x,x,x,x,x,x
         ]
 
@@ -47,9 +58,9 @@ def client_connected():
         sense.set_pixel(0, opp_bat_y + 1, white)
         sense.set_pixel(0, opp_bat_y, white)
         sense.set_pixel(0, opp_bat_y - 1, white)
-        
 
     def draw_ball():
+        global opp_bat_y
         sense.set_pixel(ball_position[0], ball_position[1], blue)
         ball_position[0] += ball_velocity[0]
         if ball_position[0] == 0:
@@ -57,23 +68,31 @@ def client_connected():
         ball_position[1] += ball_velocity[1]
         if ball_position[1] == 7 or ball_position[1] == 0:
             ball_velocity[1] = -ball_velocity[1]
-        if ball_position[0] == 6 and (bat_y-2) <= ball_position[1] <= (bat_y+2):
+
+        if ball_position[0] == 1 and (opp_bat_y - 2) <= ball_position[1] <= (
+            opp_bat_y + 2
+        ):
             ball_velocity[0] = -ball_velocity[0]
-        # if ball_position[0] == 8:
-        #     sense.set_pixels(sad)
-        #     os._exit(0)
+        if ball_position[0] == 6 and (bat_y - 2) <= ball_position[1] <= (bat_y + 2):
+            ball_velocity[0] = -ball_velocity[0]
+
+        if ball_position[0] == 0:
+            sense.set_pixels(sad)
+            server.disconnect_client()
+        elif ball_position[0] == 7:
+            sense.set_pixels(happy)
+            server.disconnect_client()
 
     def move_up(event):
         global bat_y
         if event.action == "pressed" and bat_y > 1:
             bat_y -= 1
 
-
     def move_down(event):
         global bat_y
         if event.action == "pressed" and bat_y < 6:
             bat_y += 1
- 
+
     # Main loop
     sense.stick.direction_up = move_up
     sense.stick.direction_down = move_down
@@ -82,7 +101,7 @@ def client_connected():
         draw_ball()
         sleep(0.25)
         sense.clear(0, 0, 0)
-        server.send(str(opp_bat_y))
+        server.send(str(bat_y))
 
 
 def client_disconnected():
