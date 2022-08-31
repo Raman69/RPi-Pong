a7cc3540800aaafb4dff9af7cf9f99b925f073ca
MonGoose4381 <ryanwindscheffel@gmail.com>
2022-08-31 23:38:44 +0100

    tet  Shit

--- Tetris/tetris.py
+++ Tetris/tetris.py
@@ -26,85 +26,83 @@ sense = SenseHat()
 
 
 #The x and y coords at the start (cus the whole thing is 8x8)
-x = 5
-y = 3
+x_axies = 5
+y_axies = 3
 
 
 
 #square def (yellow)
 def square():
-    sp(x,y,y)
-    sp(x+1,y,y)
-    sp(x+1,y+1,y)
-    sp(x,y+1,y)
+    sp(x_axies,y_axies,y)
+    sp(x_axies+1,y_axies,y)
+    sp(x_axies+1,y_axies+1,y)
+    sp(x_axies,y_axies+1,y)
 
 
 #line def (cyan)
 def line():
-    sp(x,y,c)
-    sp(x,y+1,c)
-    sp(x,y+2,c)
-    sp(x,y+3,c)
+    sp(x_axies,y_axies,c)
+    sp(x_axies,y_axies+1,c)
+    sp(x_axies,y_axies+2,c)
+    sp(x_axies,y_axies+3,c)
 
 
 #def t shape (pink)
 def T():
-    sp(x,y,p)
-    sp(x+1,y,p)
-    sp(x+1,y+1,p)
-    sp(x+1,y-1,p)
+    sp(x_axies,y_axies,p)
+    sp(x_axies+1,y_axies,p)
+    sp(x_axies+1,y_axies+1,p)
+    sp(x_axies+1,y_axies-1,p)
 
 
 #idk what to call this one
 #def weird shape (green)
 def weird():
 
-    sp(x,y,g)
-    sp(x+1,y,g)
-    sp(x+1,y-1,g)
-    sp(x+2,y-1,g)
+    sp(x_axies,y_axies,g)
+    sp(x_axies+1,y_axies,g)
+    sp(x_axies+1,y_axies-1,g)
+    sp(x_axies+2,y_axies-1,g)
 
 
 #L shape def (red/orange)
 def L():
-    sp(x,y,r)
-    sp(x,y+1,r)
-    sp(x+1,y,r)
-    sp(x+2,y,r)
+    sp(x_axies,y_axies,r)
+    sp(x_axies,y_axies+1,r)
+    sp(x_axies+1,y_axies,r)
+    sp(x_axies+2,y_axies,r)
 
 
 
 
-#main loop
-while True:
-    clear()
-    #gravity
-    while True:
-        time.sleep(1)
-        if x == 0:
-            break
-        else:
-            x = x-1
+
+    
 
             
     #movement left and right (joystick shit)
 
-
+while True:
+    T()
     while True:
-        for event in sense.stick.get_events():
-            direction = event.direction
-            print(event.direction)
-            if direction == "up" :
-                print("right")
-                y = y-1
-                print(y)
-            elif direction == "down":
-                print("left")
-                y = y+1
-                print(y)
+        time.sleep(1)
+        if x_axies == 0:
+            break
+        else:
+            x_axies = x_axies-1
+    for event in sense.stick.get_events():
+        direction = event.direction
+        print(event.direction)
+        if direction == "up" :
+            print("right")
+            y_axies = y_axies-1
+            print(y_axies)
+        elif direction == "down":
+            print("left")
+            y_axies = y_axies+1
+            print(y_axies)
             
-            clear()
-            T()
+        clear()
+        
 
 
 
