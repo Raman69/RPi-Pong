3304a41ff149c9877fe703c62a2353f49c8d05b1
Raman69 <abdelrahmanelrefaey5@gmail.com>
2022-09-28 21:51:24 +0100

    Main interface update to include tetris

--- Main_Interface.py
+++ Main_Interface.py
@@ -12,21 +12,52 @@ def joystick_event(event):
 
 sense.set_pixels([
     x,x,x,x,x,x,x,x,
-    x,x,x,w,w,x,x,x,
     x,x,x,x,x,x,x,x,
     x,x,x,x,x,x,x,x,
+    x,w,x,x,x,x,w,x,
+    x,w,x,x,x,x,w,x,
     x,x,x,x,x,x,x,x,
     x,x,x,x,x,x,x,x,
-    x,x,x,w,w,x,x,x,
     x,x,x,x,x,x,x,x
     ]) 
+
 event = sense.stick.wait_for_event()
-while event.direction != "up" and event.direction != "down":
+while event.direction != "right" and event.direction != "left":
     event = sense.stick.wait_for_event()
     joystick_event(event)
-if event.action == "pressed" and event.direction == "up":
-    sense.clear()
-    os.system('python /home/pi/RPi-Pong/Multiplayer_Pong/Multiplayer_server.py')
-elif event.action == "pressed" and event.direction == "down":
-    sense.clear()
-    os.system('python /home/pi/RPi-Pong/Multiplayer_Pong/Multiplayer_client.py')
+
+if event.action == "pressed" and event.direction == "right":
+    #code to run in tetris mode
+    sense.set_pixels([
+        x,x,x,x,x,x,x,x,
+        x,w,w,w,w,w,w,x,
+        x,w,w,w,w,w,w,x,
+        x,x,x,w,w,x,x,x,
+        x,x,x,w,w,x,x,x,
+        x,x,x,w,w,x,x,x,
+        x,x,x,w,w,x,x,x,
+        x,x,x,x,x,x,x,x
+        ]) 
+elif event.action == "pressed" and event.direction == "left":
+    #code to run in pong mode
+    sense.set_pixels([
+        x,x,x,x,x,x,x,x,
+        x,x,x,w,w,x,x,x,
+        x,x,x,x,x,x,x,x,
+        x,x,x,x,x,x,x,x,
+        x,x,x,x,x,x,x,x,
+        x,x,x,x,x,x,x,x,
+        x,x,x,w,w,x,x,x,
+        x,x,x,x,x,x,x,x
+        ]) 
+
+    while event.direction != "up" and event.direction != "down":
+        event = sense.stick.wait_for_event()
+        joystick_event(event)
+
+    if event.action == "pressed" and event.direction == "up":
+        sense.clear()
+        os.system('python /home/pi/RPi-Pong/Multiplayer_Pong/Multiplayer_server.py')
+    elif event.action == "pressed" and event.direction == "down":
+        sense.clear()
+        os.system('python /home/pi/RPi-Pong/Multiplayer_Pong/Multiplayer_client.py')
